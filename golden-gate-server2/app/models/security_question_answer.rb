76470b91918fd4de100740172580901daf72607d
Usman Pervaiz <de.usman@yahoo.com>
2017-09-16 17:29:09 -0400

    Completed Account Security Questions With Encryption.

--- golden-gate-server2/app/models/security_question_answer.rb
+++ golden-gate-server2/app/models/security_question_answer.rb
@@ -1,4 +1,16 @@
 class SecurityQuestionAnswer < ApplicationRecord
 	belongs_to :member
 	belongs_to :security_question
+	before_save :encrypt_security_question_answer
+
+	def encrypt_security_question_answer
+		salt = ENV['SALT'] #environment variable saved in application.yml (gem figaro)
+		key   = ActiveSupport::KeyGenerator.new('password').generate_key(salt, 32)
+		crypt = ActiveSupport::MessageEncryptor.new(key)
+		answer = self.answer
+		encrypted_answer = crypt.encrypt_and_sign(answer)
+		self.answer = encrypted_answer
+	end
+
 end
+
